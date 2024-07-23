9552d4889bcb2c8126a2299c661798a27d143af9
Abhangipratigna <pratigna2004@gmail.com>
2024-07-23 15:51:17 +0530

    Add files via upload

---
+++ Answer3.11.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+
+
+int main()
+
+{
+
+int names;
+
+//int sum;
+
+printf("\nnames: ");
+
+printf("\nEnter the names niraj");
+printf("\nEnter the names krisha");
+printf("\nEnter the names jay");
+printf("\nEnter the  names het");
+printf("\nEnter the  names hiyan");
+
+
+return 0;
+
+}
+
